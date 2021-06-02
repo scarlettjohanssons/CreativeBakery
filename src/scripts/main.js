979830b4d3081f8e772b111dd17fe0b5613d4f6f
scarlettjohanssons <wolverinelop@gmail.com>
2021-06-02 10:56:10 +0300

    picture fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,37 @@
-// 'use strict';
+'use strict';
+/* eslint-disable */
 
-// window.onload = function() {
-//   let btn1 = document.querySelector("#ch1");
-//   let btn2 = document.querySelector("#ch2");
-//   let btn3 = document.querySelector("#ch3");
+window.onload = function() {
+  const btn1 = document.querySelector(".header__bg-slider-number--01");
+  const btn2 = document.querySelector(".header__bg-slider-number--02");
+  const btn3 = document.querySelector(".header__bg-slider-number--03");
+  const btn4 = document.querySelector(".header__slider-number--01");
+  const btn5 = document.querySelector(".header__slider-number--02");
+  const btn6 = document.querySelector(".header__slider-number--03");
 
-//   btn1.addEventListener('click', () => {
-//     document.querySelector(".header").style.backgroundColor = "#444";
-//   })
-//   btn2.addEventListener('click', () => {
-//     document.querySelector(".header").style.backgroundColor = "#000";
-//   })
-//   btn3.addEventListener('click', () => {
-//     document.querySelector(".header").style.backgroundColor = "#111";
-//   })
-// }
+  btn1.addEventListener('click', () => {
+    document.querySelector(".header").style.backgroundImage = "url(/head.c6782c52.jpg)";
+    document.querySelector('.header__description').style.color = '#412d22';
+  })
+  btn2.addEventListener('click', () => {
+    document.querySelector(".header").style.backgroundImage = "url(/bakery2.8448b89b.png)";
+    document.querySelector('.header__description').style.color = '#e39a3b';
+  })
+  btn3.addEventListener('click', () => {
+    document.querySelector(".header").style.backgroundImage = "url(/bakery3.06d04078.png)";
+    document.querySelector('.header__description').style.color = '#412d22';
+  })
+
+  btn4.addEventListener('click', () => {
+    document.querySelector(".header").style.backgroundImage = "url(/head.c6782c52.jpg)";
+    document.querySelector('.header__description').style.color = '#412d22';
+  })
+  btn5.addEventListener('click', () => {
+    document.querySelector(".header").style.backgroundImage = "url(/bakery2.8448b89b.png)";
+    document.querySelector('.header__description').style.color = '#e39a3b';
+  })
+  btn6.addEventListener('click', () => {
+    document.querySelector(".header").style.backgroundImage = "url(/bakery3.06d04078.png)";
+    document.querySelector('.header__description').style.color = '#412d22';
+  })
+}
